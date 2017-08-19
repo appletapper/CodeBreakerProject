7cdfec734fd4cb8a27187965b1fc2e79e0481f3c
appletapper <31152064+appletapper@users.noreply.github.com>
2017-08-19 18:10:32 +1000

    new functions added(3)

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,12 +3,31 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+
+    //new project addititions
+    if(answer.value == '' || attempts.value == '') {
+      setHiddenFields();
+    }
 }
 
 //New project functions...
 
 //setHiddenFields calculates a random number between 0 to 9999 (inclusive)
 function setHiddenFields() {
-  answer.value = Math.floor(Math.random() * 10000);
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while(answer.value.length < 4) {
+    answer.value = "0" + answer.value;
+  }
+  attempt.value = "0";
+}
+
+function setMessage(message) {
+  document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input) {
+  if(input.length != 4) {
+    setMessage('Guesses must exactly four numbers long, or else prepare to get your butt kicked! ;-)');
+    return false;
+  }
 }
